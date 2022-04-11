c6a547c83f365e09848f7d598494b9bcd57c2cd4
thomas588 <thomasthompson0204@gmail.com>
2022-04-11 15:15:44 +0500

    replay_firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from "./component/Header/Header";
+// import Header from "./component/Header/Header";
 // import TopForms from './component/TopForms/TopForms';
 import Home from './component/Home/Home'
 import Login from './component/Auth/Login/Login';
+// import Wrapper from './component/Wrapper/Wrapper'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-        <Header />
+        {/* <Header /> */}
         {/* <TopForms /> */}
 
 
-        <Router>
           <header>
           <Routes>
             <Route path='/home' element={<Home />}/>
             <Route path='/login' element={<Login />}/>
+            {/* <Route path='wrapper' element={<Wrapper />}/> */}
           </Routes>
           </header>
-        </Router>
     </div>
+    </Router>
   );
 }
 
--- src/component/Auth/Login/Login.js
+++ src/component/Auth/Login/Login.js
@@ -1,37 +1,11 @@
-import React, { useState } from 'react'
-import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom'
+import React from "react";
 
-export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    let navigate = useNavigate()
-    let auth = getAuth()
-
-    const loginUser = async (event) => {
-        event.preventDefault();
-        try {
-            await signInWithEmailAndPassword(auth,email, password).then(() => navigate('/profile')).catch(e => console.log(e))
-        } catch (error) {
-            alert(error.code)
-        }
-    }
-
-    return (
-      <div>
-        <h1>Login Page</h1>
-        <form onSubmit={loginUser}>
-            <input type={'email'} placeholder={'email'} name={'email'} onChange={(v) => setEmail(v.target.value)}/>
-            <input type={'password'} placeholder={'password'} name={'password'} onChange={(v) => setPassword(v.target.value)}/>
-            <button type='submit'>Login</button>
-        </form>
-        <br/>
-        <a href='/signup'>Need an account</a>
-      </div>
-    )
+function Login() {
+    return(
+        <>
+        Login component
+        </>
+    );
 }
 
-// sign => up регистрация
-// log in => войдите в систему
-// log out => выйти из системы
+export default Login;
