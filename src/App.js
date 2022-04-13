731c236f68e402d6ee7843b733d0ac3ba2a80d91
thomas588 <thomasthompson0204@gmail.com>
2022-04-13 16:04:40 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from "./component/Header/Header";
+// import Header from "./component/Header/Header";
 import MainPage1 from './component/MainPage/MainPage1/MainPage1'
 import Login from './component/Auth/Login/Login';
 
